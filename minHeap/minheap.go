09ab22142107d76251baffd1e96c76fc13b5e043
rituparnakhaund <rituparna.k98@gmail.com>
2021-08-25 13:20:18 +0530

    Divided into different directories

--- minHeap/minheap.go
+++ minHeap/minheap.go
@@ -3,14 +3,14 @@ package main
 import "time"
 
 type FileInfoaccess struct {
-	name string
-	last_access_time time.Time
+	Name string
+	Last_access_time time.Time
 }
 func Push(info FileInfoaccess) error {
 	return nil
 }
 
 func Pop() (FileInfoaccess ,error) {
-	return FileInfoaccess{name : "hello"}, nil
+	return FileInfoaccess{Name : "hello"}, nil
 
 }
